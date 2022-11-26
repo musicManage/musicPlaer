2eb31f24949eeffe5a2ab10663cc204de38c8f0f
JiePlus <780075291@qq.com>
2022-11-26 18:33:21 +0800

    dev

--- msclient/src/views/HomeView.vue
+++ msclient/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
+    <el-button type="primary">414354</el-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
