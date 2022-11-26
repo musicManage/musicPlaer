a476323d9add3d5a4b652e01932a0f149529fb81
nber666 <522691223@qq.com>
2022-11-26 23:12:37 +0800

    Default Changelist

---
+++ msclient/src/assets/data/swiper.js
@@ -0,0 +1,10 @@
+//轮播图的数据
+const swiperList = [
+    {picImg: require('@/assets/img/swiper/1.jpg')},
+    {picImg: require('@/assets/img/swiper/2.jpg')},
+    {picImg: require('@/assets/img/swiper/3.jpg')},
+
+]
+export {
+    swiperList
+}
---
+++ msclient/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <swiper></swiper>
+  </div>
+</template>
+
+<script>
+import Swiper from "@/components/Swiper";
+export default {
+  name: "home",
+  components:{
+    Swiper
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
